1280d0b1cd25f0dd7a9d9ebe631b6ecb5fbc0e7c
Daniel Martinez <daniellhf@icloud.com>
2023-06-18 21:11:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <input type="text" placeholder="question"/>
-      <button>Submit</button>
+      <input type="text" placeholder="question" style={{border:'none'}}/>
+      <button style={{background: 'transparent', borderRadius: 5}}>Submit</button>
     </div>
   );
 }
